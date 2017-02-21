8c0fe3fb68d185e0d7cf5840524a7d2f5755477d
jermiedomingo <8jermiedomingo@gmail.com>
2017-02-21 23:04:38 +0400

    navigator.mediaDevices.enumerateDevices sample

--- user-media/main.js
+++ user-media/main.js
@@ -6,15 +6,43 @@ const hasUserMedia = () => {
 }
 
 let video = document.querySelector('video');
+let audioSource = null;
+let videoSource = null;
 
+//callback function for success in navigator.mediaDevices.enumerateDevices()
+let gotDevices = (sources) => {
+
+	for(let source of sources){
+
+		if(source.kind === 'audioinput'){
+			console.log('Microphone found:', source.label, source.deviceId);
+			audioSource = source.deviceId;
+
+		} else if(source.kind === 'videoinput'){
+			console.log('Camera found:', source.label, source.deviceId);
+			videoSource = source.deviceId;
+
+		} else {
+			console.log('Other source found:' , source);
+		}
+	}
+}
+
+//show all the media devices available for the user
+navigator.mediaDevices.enumerateDevices().then(gotDevices).catch((error) => { console.log(error);});
+
+//specifying constraints for the UserMedia
 let constraints = {
 	 	video: {
 	 		mandatory: {
 	 			minWidth: 640,
 	 			minHeight: 480
-	 		}
+	 		},
+	 		optional: [{sourceId: videoSource}]
 	 	},
-	 	audio: false
+	 	audio: {
+	 		optional: [{sourceId: audioSource}]
+	 	}
 	};
 
 	//very simple still unreliable user agent checker if mobile
@@ -34,15 +62,12 @@ let constraints = {
 	}
 
 
+//getUserMedia success
 const successCallback = (stream) => {
-	
-	if(window.URL) {
-		video.src = window.URL.createObjectURL(stream);
-	} else {
-		video.src = stream;
+		video.srcObject = stream;
 	}
-}
 
+//getUserMedia error
 const errorCallback = (error) => {
 	console.log('navigator.getUserMedia error: ', error);
 }
